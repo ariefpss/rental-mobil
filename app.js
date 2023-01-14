a11eb2c41959aa98c46d869d5896f701d3556021
ariefpss <permanaarief8@gmail.com>
2023-01-14 15:02:34 +0900

    modified:   app.js 	modified:   app/controllers/indexController 	modified:   package.json 	modified:   public/images/car-landing-page.png 	modified:   public/stylesheets/custom-bootstrap.min.css 	modified:   public/stylesheets/custom-bootstrap.min.css.map 	modified:   views/index.ejs 	modified:   views/partials/header.ejs

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js/')))
+app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
